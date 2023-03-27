d69dd3d871b3096151e737d63faf24bb15c425ae
YooHyeok <yjk7454@naver.com>
2023-03-28 01:49:17 +0900

    핸들러 어댑터 구현 클래스 v3 추가

--- src/main/java/hello/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -10,5 +10,5 @@ import java.io.IOException;
 public interface MyHandlerAdapter {
     boolean supports(Object handler); // 컨트롤러가 넘어왔을 때 Handler 지원 여부
 
-    ModelView handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
 }
