e68b82232b5a70bcae0121b9025723fc3f2fba1b
YooHyeok <yjk7454@naver.com>
2023-03-27 21:16:27 +0900

    회원 등록 컨트롤러 추가

---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/Controller/MemberFormControllerV1.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.frontcontroller.v1.Controller;
+
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 회원 등록 컨트롤러
+ */
+public class MemberFormControllerV1 implements ControllerV1 {
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MemberFormControllerV1.process");
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);//controller에서 view로 이동할때 사용.
+        dispatcher.forward(request,response); // jsp를 찾아서 호출한다.
+    }
+}
