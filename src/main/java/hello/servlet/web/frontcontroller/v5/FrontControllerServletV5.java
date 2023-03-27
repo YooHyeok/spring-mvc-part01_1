3f97398b34df4371f8bf989623c61e961dc4911e
YooHyeok <yjk7454@naver.com>
2023-03-28 01:50:01 +0900

    FrontController Version5 추가 - ver3의  ModelView방식 Adapter로직 구현

---
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,89 @@
+package hello.servlet.web.frontcontroller.v5;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+    private final Map<String, Object> handlerMappingMap = new HashMap<>(); // Value 타입이 Object이므로 v1 ~ v5까지 다양하게 가능
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public FrontControllerServletV5() {
+        handlerMappingMap();
+        initHandlerAdapters();
+    }
+
+    private void handlerMappingMap() {
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+    }
+
+    private void initHandlerAdapters() {
+        handlerAdapters.add(new ControllerV3HandlerAdapter());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Object handler = getHandler(request); //handler 찾아오기
+        if (handler == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        MyHandlerAdapter adapter = getHandlerAdapter(handler); //handler adapter 찾아오기
+
+        ModelView mv = adapter.handle(request, response, handler); //handle() : createParamMap, process 작업 진행
+
+        String viewName = mv.getViewName();
+        MyView view = viewResolver(viewName);
+        view.render(mv.getModel(), request, response);
+    }
+
+    private MyHandlerAdapter getHandlerAdapter(Object handler) {
+        for (MyHandlerAdapter adapter : handlerAdapters) {
+            if (adapter.supports(handler)) { //만약 handler가 adapter를 지원한다면 (V3)
+                return adapter;
+            }
+        }
+        throw new IllegalAccessError( "[" + handler + "] Handler Adapter를 찾을 수 없습니다.");
+    }
+
+    /**
+     * 요청정보를 통해 handler 반환
+     * @param request
+     * @return
+     */
+    private Object getHandler(HttpServletRequest request) {
+        String requestURI = request.getRequestURI(); //localhost:포트번호/ 이후의 주소
+        Object handler = handlerMappingMap.get(requestURI);
+        return handler;
+    }
+
+    /**
+     * 매개변수를 request와 response대신 paramMap이라는 Map객체로넘겨야하므로 request객체로부터 파라미터를 추출한다
+     * @param viewName
+     * @return
+     */
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+}
