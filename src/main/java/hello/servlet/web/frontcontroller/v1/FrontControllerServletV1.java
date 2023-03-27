8e72841a6ef784600fa1eedbfce6653c037912e2
YooHyeok <yjk7454@naver.com>
2023-03-27 22:11:03 +0900

    FrontController Version1 구현

---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -0,0 +1,40 @@
+package hello.servlet.web.frontcontroller.v1;
+
+import hello.servlet.web.frontcontroller.v1.Controller.MemberFormControllerV1;
+import hello.servlet.web.frontcontroller.v1.Controller.MemberListControllerV1;
+import hello.servlet.web.frontcontroller.v1.Controller.MemberSaveControllerV1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*") // v1디렉토리 하위의 모든 url경로
+public class FrontControllerServletV1 extends HttpServlet {
+
+    private Map<String, ControllerV1> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV1() {
+        controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
+        controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+        controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV1.service");
+        String requestURI = request.getRequestURI(); //localhost:포트번호/ 이후의 주소
+        ControllerV1 controller = controllerMap.get(requestURI); //FrontControllerServletV1 생성자에 의해 controllerMap으로 부터 uri와 일치하는 key의 Value newInstance 객체를 반환한다.
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        controller.process(request, response); // 다형성에 의해 3 객체중 선택된 인스턴스 객체에 선언된 process() 오버라이딩 메소드가 호출된다.
+
+    }
+}
