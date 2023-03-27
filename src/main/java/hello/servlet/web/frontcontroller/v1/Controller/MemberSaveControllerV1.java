f6f28fbb6374f824b267b275a6b1a2063b03194a
YooHyeok <yjk7454@naver.com>
2023-03-27 21:18:14 +0900

    회원 저장 컨트롤러 추가

---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/Controller/MemberSaveControllerV1.java
@@ -0,0 +1,33 @@
+package hello.servlet.web.frontcontroller.v1.Controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 회원 저장 컨트롤러
+ */
+public class MemberSaveControllerV1 implements ControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MemberSaveControllerV1.process");
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member); //회원 가입 (저장)
+
+        //Model에 Data 보관
+        request.setAttribute("member", member); //request객체의 내부 저장소(map)에 저장
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        request.getRequestDispatcher(viewPath).forward(request,response);
+    }
+}
