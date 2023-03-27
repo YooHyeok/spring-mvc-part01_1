ac463deda07a298422965e6f794d8f1847a51ae4
YooHyeok <yjk7454@naver.com>
2023-03-27 23:10:17 +0900

    회원 등록 컨트롤러 Version3 추가

---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberFormControllerV3.java
@@ -0,0 +1,13 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.Map;
+
+public class MemberFormControllerV3 implements ControllerV3 {
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        return new ModelView("new-form"); // 인스턴스 생성 및 논리 이름 초기화
+    }
+}
