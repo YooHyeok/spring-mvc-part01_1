e1c53115bc7ade40c2c71c0c67dbc2dd7c898303
YooHyeok <yjk7454@naver.com>
2023-03-28 17:13:39 +0900

    Spring 제공 실용적인 기능사용 회원 컨트롤러 구현 - version3

---
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,53 @@
+package hello.servlet.web.springmvc.v3;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * Spring 제공 실용적인 기능
+ * 1. 매핑주소 v2 -> v3로 변경
+ * 2. 각 매핑 메소드의 반환타입을 ModelAndView에서 String으로 변경
+ * 3. request.getParameter 대신 @RequestParam 어노테이션 활용
+ * 4. ModelAndView 대신 Model객체 활용
+ * 5. RequestMapping을 Get방식 Post방식 각각의 메소드방식에 맞는 어노테이션으로 변경
+ */
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+
+    @RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    //@GetMapping("/new-form") 으로 대체 가능
+    public String newForm() {
+        System.out.println("SpringMemberControllerV3.newForm");
+        return "new-form";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    //@PostMapping("/save")으로 대체 가능
+    public String save(Model model, @RequestParam("username") String username, @RequestParam("age") int age) {
+        System.out.println("SpringMemberControllerV3.save");
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+        model.addAttribute("member", member);
+        return "save-result";
+    }
+
+    @GetMapping
+    public String members(Model model) {
+        System.out.println("SpringMemberControllerV3.members");
+        List<Member> members = memberRepository.findAll();
+        model.addAttribute("members", members);
+        return "members";
+    }
+}
