480f8725c8ee59cd557072985e3884d217080965
YooHyeok <yjk7454@naver.com>
2023-03-28 15:01:31 +0900

    ViewResolver Bean등록으로 직접 설정

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,8 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.util.Arrays;
 
@@ -12,5 +14,14 @@ public class ServletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
+	}
+
+	/**
+	 * ViewResolver 설정 직접 Bean으로 등록
+	 * @return
+	 */
+	@Bean
+	InternalResourceViewResolver internalResourceViewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
 	}
 }
