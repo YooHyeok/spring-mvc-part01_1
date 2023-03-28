81718edd383bd3a70a1c96d3e0e7655b7bb719c1
YooHyeok <yjk7454@naver.com>
2023-03-28 15:21:39 +0900

    InternalResourceViewResolver Bean설정 주석 정리

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -18,6 +18,10 @@ public class ServletApplication {
 
 	/**
 	 * ViewResolver 설정 직접 Bean으로 등록
+	 * InternalResourceViewResolver 는 InternalResourceView를 반환한다. ( buildView() )
+	 * InternalResourceView는 jsp처럼 forward()를 호출하는 경우에 사용한다.
+	 * (만약 JSTL 라이브러리가 있으면 InternalView를 상속받은 JstlView를 반환한다. - jstl사용시 약간의 부가기능 추가)
+	 * view.render가 호출되고 InternalResourceView는 forward를 사용해서 JSP를 실행한다. ( renderMergedOutputModel() )
 	 * @return
 	 */
 	@Bean
